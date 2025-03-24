74a7753ab94a69b9503dc92fb59455e30ac003dd
Mr-76 <bartmoss77@tutanota.com>
2025-03-24 00:19:30 -0300

    update code,with comments, and ad new simple loop

--- hideInfo.py
+++ hideInfo.py
@@ -13,30 +13,27 @@ message = "The I took the one less traveled by,And that has made all the differe
 #may need to add padding if the value if not multiple of 3....
 ascii_values = [ord(c) for c in message]
 
-new_img = np.array(img)
-index = 0
-placing_of_chars = {}
-pointsofnoise = 0
-for y in range(height):
-    for x in range(len(ascii_values)):
-        if (index < len(ascii_values)-1):
+
+new_img = np.array(img) #Load image as array
+index = 0 #index to jump
+placing_of_chars = {} #map of x and y to get chars back from image
+pointsofnoise = 100 #limiter of points of noise
+for x in range(len(ascii_values)): #loop in the number of chars of the message
+    if (index < len(ascii_values)-1):
+        randomX = random.randint(0, width - 1)
+        randomY = random.randint(0, height - 1)
+        new_img[randomY, randomX] = [ascii_values[index], ascii_values[index+1],ascii_values[index+2]]
+        placing_of_chars[index] = (randomX,randomY) #makes you get the order and coord of the ascii values
+        index+=3 #well can be any number of char but 3 by 3 makes it use the rgb values inside each pixel.
+    else:
+        #Still need to verify if the random value was alredy used to store a char
+        for x in range(pointsofnoise): #loop in the number of chars of the message
             randomX = random.randint(0, width - 1)
             randomY = random.randint(0, height - 1)
-            new_img[randomY, randomX] = [ascii_values[index], ascii_values[index+1],ascii_values[index+2]]
-            placing_of_chars[index] = (randomX,randomY)
-            time.sleep(0.05) 
-            index+=3
-        else:
-            if (pointsofnoise < 1000):
-                pointsofnoise += 1
-                randomX = random.randint(0, width - 1)
-                randomY = random.randint(0, height - 1)
-                r = random.randint(0, 255)
-                g = random.randint(0, 255)
-                b = random.randint(0, 255)
-                new_img[randomY, randomX] = [r,g,b]
-            continue
-
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            new_img[randomY, randomX] = [r,g,b]
 print(placing_of_chars)
 modified_image = Image.fromarray(new_img)
 modified_image.save('modified_fruit.jpg')
--- imageSubtract.py
+++ imageSubtract.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 #with this you can clearly see the image noise and info.... if u can find it xd
-
 image1 = cv2.imread('fruit.jpg')
 image2 = cv2.imread('modified_fruit.jpg')
 
@@ -10,5 +9,6 @@ if image1.shape != image2.shape:
     raise ValueError("Images must have the same dimensions for subtraction.")
 
 result_image = cv2.absdiff(image1, image2)
+
 
 cv2.imwrite('subtracted_image.jpg', result_image)
